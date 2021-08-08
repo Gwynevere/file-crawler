2601a6a5fa36d5989f9b702781658decebbbdfb3
Gwynevere <lahrabzimohamed@gmail.com>
2021-08-08 17:41:37 +0100

    updated project structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import {v5 as uuidv5} from "uuid";
-import {ParsedPathID, SEARCH_TYPES, WalkInputOptions} from "../typings";
+import {ParsedPathID, SEARCH_TYPES, WalkInputOptions} from "./typings";
 
 const fs = require('fs');
 const path = require('path');
-
 
 async function walk({
                         dir,
